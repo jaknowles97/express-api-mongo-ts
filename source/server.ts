3152d61ef16a1a669fea3c74b045aec555d8d7b5
jaknowles97 <jaknowles1997@gmail.com>
2021-03-22 12:16:33 -0400

    didn't save server.ts before pushing commit

--- source/server.ts
+++ source/server.ts
@@ -13,3 +13,36 @@ router.use((req, res, next) => {
         logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}], STATUS - [${res.statusCode}]`);
     });
 });
+//   Parse the request
+router.use(express.urlencoded({ extended: false }));
+router.use(express.json());
+
+//  API Rules
+router.use((req, res, next) => {
+    // Access-Control-Allow-Origin is only for development purposes (it allows requests to come from anywhere). All routes and ips should be predefined in production!!!
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+/** Routes go here */
+// router.use('/api/sample', sampleRoutes);
+
+/** Error handling */
+router.use((req, res, next) => {
+    const error = new Error('Not found');
+
+    res.status(404).json({
+        message: error.message
+    });
+});
+
+const httpServer = http.createServer(router);
+
+httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running ${config.server.hostname}:${config.server.port}`));
